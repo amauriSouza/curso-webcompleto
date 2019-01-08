547acc9602d60be6ebeefdd07ab40785205bb4ed
Amauri Souza <amauri.teixeira@kognit.com.br>
2019-01-08 11:09:59 -0200

    Aulas JavaScript

---
+++ JavaScript/fundamentos/7templateString.js
@@ -0,0 +1,16 @@
+const nome = "Amauri";
+
+const concat = "Ola " + nome + "!";
+
+console.log(concat);
+
+console.log(`
+  Ola 
+  ${nome}`);
+
+console.log(`${2 + 2}`);
+
+
+const up = texto => texto.toUpperCase()
+
+console.log(`${nome}, ${up('cuidado')}!`);
